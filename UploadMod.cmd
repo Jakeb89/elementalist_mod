53bb28884b51ea3526988668bed4daa8e9c29926
Jakeb89 <1346278+Jakeb89@users.noreply.github.com>
2019-02-13 20:03:33 -0800

    Delete UploadMod.cmd

--- UploadMod.cmd
+++
@@ -1,3 +0,0 @@
-cd C:\Program Files (x86)\Steam\steamapps\common\SlayTheSpire
-copy mods\ElementalistMod.jar TheElementalist\content\
-.\jre\bin\java.exe -jar .\mod-uploader.jar upload -w=TheElementalist
